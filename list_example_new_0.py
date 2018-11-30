70bfcfd940baa968f1e9b1e18fa104d23d6e154c
ElonMusk2018 <clogbenner@hotmail.com>
2018-11-30 10:00:52 -0800

    AA

---
+++ list_example_new_0.py
@@ -0,0 +1,8 @@
+#list_example_two_0.py lb
+filelist1 = ["IMG_001.png","IMG_007.png", "IMG_011.png", "IMG_777.png"]
+filelist2 = ["IMG_001.png","IMG_007.png", "IMG_011.png", "IMG_777.png"]
+
+for i in range (0,4):
+    #print(filelist1[i],fileslist[i])
+    print ("mv "+filelist1[i])+" "+filelist2[i]+":;")
+    
