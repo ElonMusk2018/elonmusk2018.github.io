8829211654c08f2038e1d9a2b2feead65720a16f
lb <lb@echo.ksd.edu>
2019-06-07 10:28:44 -0700

    AA

---
+++ html/My-first-Iframe-Gallery.html/gitregister.sh
@@ -0,0 +1,28 @@
+body {
+	background-color: powderblue;
+}
+h2 {
+	color: blue;
+	font-family: vcourier;
+	font-size: 300%;
+}
+p {
+	border: 1px solide powderblue;
+
+table {
+	border-collapse: collapse;
+}
+
+table th {
+	border: left;
+	background-color: #3a6070;
+	color: #FFF;
+	font-size: 300%;
+}
+
+table td {
+	border: 1px solid #e3e3e3;
+	padding: 4px 8px;
+	font-family: courier;
+	font-size: 300%;
+}
